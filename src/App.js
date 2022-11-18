8dd957414c8f421b6ce6de1b559deaa5a7cafeb3
Paulo Prata <pauloprata10@gmail.com>
2022-11-18 17:45:05 -0300

    projeto theme toggler - context api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from './contexts/theme-context'
+import { ThemeTogglerButton } from './components/theme-toggler-button/theme-toggler-button'
+import { Card } from './components/card/card'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <ThemeTogglerButton />
+      <Card />
+      <Card />
+      <Card />
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default App
---
+++ src/contexts/theme-context.js
@@ -0,0 +1,26 @@
+import { createContext, useState } from "react"
+
+export const themes = {
+    light: {
+        color: '#000000',
+        background: '#eeeeee'
+    },
+    dark: {
+        color: '#ffffff',
+        background: '#000000'
+    }
+
+}
+
+export const ThemeContext = createContext({})
+
+export const ThemeProvider = (props) => {
+
+    const [ theme, setTheme ] = useState(themes.light)
+
+    return(
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
+}
